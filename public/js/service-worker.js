9e1d18819a617ee51a317c7215ec4471b6e8c4ce
Emma Mattner <emattner1997@gmail.com>
2021-07-19 16:31:50 +0930

    adding eventlistener

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -17,8 +17,25 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
         caches
             .open(CACHE_NAME)
-            .then((cache) => { cache.addAll(FILES_TO_CACHE))
+            .then((cache) => cache.addAll(FILES_TO_CACHE))
             .then(self.skipWaiting())
     );
 });
 
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches
+            .keys()
+            .then((keyList) => {
+                return Promise.all(
+                    keyList.map((key) => {
+                        if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                            return caches.delete(key);
+                        }
+                    })
+                );
+            })
+    );
+    self.clients.claim()
+});
